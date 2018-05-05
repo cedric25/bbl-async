0d3239c2999596e253478d250f88b31a146e0d2c
Cedric Nicoloso <cnicoloso@octo.com>
2018-05-05 19:13:29 +1000

    async / await good and bad example

---
+++ async-await/badCode.js
@@ -0,0 +1,33 @@
+// Run with `time node async-await/badCode.js`
+
+async function asyncAwaitColors() {
+
+  console.log('Calling asynchronous code...')
+
+  try {
+    const colorOne = await getHexCodeForColor('blue')
+    const colorTwo = await getHexCodeForColor('red')
+    console.log('colorOne:', colorOne)
+    console.log('colorTwo:', colorTwo)
+  } catch (err) {
+    console.error(err)
+  }
+
+}
+
+function getHexCodeForColor(color) {
+
+  const colorCode = color === 'blue' ? '#0000ff' : '#ff0000'
+
+  return new Promise((resolve, reject) => {
+    // Here just simulating a wait time
+    setTimeout(() => {
+      resolve(colorCode)
+    }, 800)
+  })
+
+}
+
+(async function run() {
+  await asyncAwaitColors()
+})()
---
+++ async-await/goodCode.js
@@ -0,0 +1,35 @@
+// Run with `time node async-await/goodCode.js`
+
+async function asyncAwaitColors() {
+
+  console.log('Calling asynchronous code...')
+
+  try {
+    const colors = await Promise.all([
+      getHexCodeForColor('blue'),
+      getHexCodeForColor('red'),
+    ])
+    console.log('colorOne:', colors[0])
+    console.log('colorTwo:', colors[1])
+  } catch (err) {
+    console.error(err)
+  }
+
+}
+
+function getHexCodeForColor(color) {
+
+  const colorCode = color === 'blue' ? '#0000ff' : '#ff0000'
+
+  return new Promise((resolve, reject) => {
+    // Here just simulating a wait time
+    setTimeout(() => {
+      resolve(colorCode)
+    }, 800)
+  })
+
+}
+
+(async function run() {
+  await asyncAwaitColors()
+})()
