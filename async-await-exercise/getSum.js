33584146bd6f728a03f7a2e1a85a8e4c50a891ff
Cedric Nicoloso <cnicoloso@octo.com>
2018-05-22 22:32:47 +1000

    Default behavior is to resolve

--- async-await-exercise/getSum.js
+++ async-await-exercise/getSum.js
@@ -3,14 +3,14 @@ function getSum(numberOne, numberTwo) {
   return new Promise((resolve, reject) => {
 
     // Here just simulating a wait time
-    // setTimeout(() => {
-    //   resolve(numberOne + numberTwo)
-    // }, 800)
+    setTimeout(() => {
+      resolve(numberOne + numberTwo)
+    }, 800)
 
     // Reject after a delay
-    setTimeout(() => {
-      reject(new Error('Add 2 numbers is too hard for me :('))
-    }, 800)
+    // setTimeout(() => {
+    //   reject(new Error('Add 2 numbers is too hard for me :('))
+    // }, 800)
   })
 
 }
