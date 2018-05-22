52ae6f0c8e3077fd153c8e60b8eee9d04d37aa90
Cedric Nicoloso <cnicoloso@octo.com>
2018-05-22 22:33:15 +1000

    Add run command with 'time' + add time limit comment

--- async-await-exercise/exercise.js
+++ async-await-exercise/exercise.js
@@ -1,6 +1,8 @@
 const { getOneNumber } = require('./getOneNumber')
 const { getSum } = require('./getSum')
 
+// time node async-await-exercise/exercise.js
+
 /**
  * Instructions:
  *  - Call getOneNumber() two times
@@ -11,6 +13,7 @@ const { getSum } = require('./getSum')
  *  - getOneNumber() and getSum() are asynchronous and return promises!
  *  - Sometimes getOneNumber() will throw an error, handle it properly.
  *    (= I don't want to see any "UnhandledPromiseRejectionWarning" in the console!)
+ *  - It should not take more than 2 seconds
  */
 
 // TODO START CODING HERE
